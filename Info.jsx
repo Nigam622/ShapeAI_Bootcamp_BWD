20fee1dd60ba03ca72b809fd3f8d593a03d5ed72
Nigam622 <80585462+Nigam622@users.noreply.github.com>
2021-06-26 20:52:56 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web Dev React.js Bootcamp</p>
+      <p>It was a superb Bootcamp taken up by Shaurya Sinha.</p>
+      <p>We covered HTML,CSS,JS and React.js</p>
+    </div>
+  );
+}
+
+export default Info;
